8b02e61116431206c35d2dbd2987b3b62ebb8f1b
Zorbn <public.zorb@gmail.com>
2023-02-04 19:32:05 -0800

    Fixed xorshift

--- src/rng.rs
+++ src/rng.rs
@@ -11,9 +11,10 @@ impl Rng {
         }
     }
 
+    // Get a random number using 32bit xorshift.
     pub fn range(&mut self, max: u32) -> u32 {
         self.state ^= self.state << 13;
-        self.state ^= self.state >> 7;
+        self.state ^= self.state >> 17;
         self.state ^= self.state << 5;
         self.state % max
     }
